7633780b10079b7f775d34260092cadc1f2b137e
zhouchong0910du@163.com <zhouchong0910du@163.com>
2020-12-04 21:59:13 +0800

    增加登陆页

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div style="display: flex; justify-content: center; margin-top: 150px">
+      <el-card style="width: 400px">
+        <div slot="header" class="clearfix">
+          <span>登录</span>
+        </div>
+        <table>
+          <tr>
+            <td>用户名</td>
+            <td>
+              <el-input
+                v-model="user.username"
+                placeholder="请输入用户名"
+              ></el-input>
+            </td>
+          </tr>
+          <tr>
+            <td>密码</td>
+            <td>
+              <el-input
+                type="password"
+                v-model="user.password"
+                placeholder="请输入密码"
+                @keydown.enter.native="doLogin"
+              ></el-input>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <el-radio v-model="radio" label="1">我是老师</el-radio>
+              <el-radio v-model="radio" label="2">我是学生</el-radio>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <el-button style="width: 300px" type="primary" @click="doLogin"
+                >登录</el-button
+              >
+            </td>
+          </tr>
+        </table>
+      </el-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      radio: "1",
+      user: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    doLogin() {
+      alert(JSON.stringify(this.user));
+    }
+  }
+};
+</script>
