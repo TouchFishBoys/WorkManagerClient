9e8c4724f990525ddd2f397a4d7420f6f69dcacc
zhouchong0910du@163.com <zhouchong0910du@163.com>
2020-12-05 12:53:50 +0800

    Update FileList

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -74,48 +74,6 @@ export default {
               ]
             }
           ]
-        },
-        {
-          label: "一级 2",
-          children: [
-            {
-              label: "二级 2-1",
-              children: [
-                {
-                  label: "三级 2-1-1"
-                }
-              ]
-            },
-            {
-              label: "二级 2-2",
-              children: [
-                {
-                  label: "三级 2-2-1"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          label: "一级 3",
-          children: [
-            {
-              label: "二级 3-1",
-              children: [
-                {
-                  label: "三级 3-1-1"
-                }
-              ]
-            },
-            {
-              label: "二级 3-2",
-              children: [
-                {
-                  label: "三级 3-2-1"
-                }
-              ]
-            }
-          ]
         }
       ],
       form = 0,
