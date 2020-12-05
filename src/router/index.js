18e4a005278b257582c7956f5f3319b2feffcd5f
zhouchong0910du@163.com <zhouchong0910du@163.com>
2020-12-05 10:46:59 +0800

    add FileList(somethine wrong)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../components/Login.vue";
 import Upload from "../components/Upload.vue";
+import FileList from "../components/FileList.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/upload",
     name: "Upload",
     component: Upload
+  },
+  {
+    path: "/filelist",
+    name: "FileList",
+    component: FileList
   },
   {
     path: "/about",
