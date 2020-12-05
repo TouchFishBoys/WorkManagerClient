dfbf13dace4705b4689e5ee5c7c6e3b124abcd9f
zhouchong0910du@163.com <zhouchong0910du@163.com>
2020-12-05 12:51:17 +0800

    add FileList(something wrong)

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,83 +1,52 @@
 <template>
   <el-container style="height: 500px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['1', '3']">
-        <el-submenu index="1">
-          <template slot="title"
-            ><i class="el-icon-message"></i>导航一</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title"
-            ><i class="el-icon-setting"></i>导航三</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
+      <div>
+        <el-switch
+          style="display: block"
+          v-model="form"
+          active-color="#13ce66"
+          inactive-color="#13b9ce"
+          :active-text="switchText1"
+          :inactive-text="switchText2"
+        >
+        </el-switch>
+      </div>
+      <el-tree
+        :data="menu"
+        :props="defaultProps"
+        accordion
+        @node-click="handleClickMenu"
+      ></el-tree>
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-
       <el-main>
         <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
+          <el-table-column
+            v-for="header in tableHeader"
+            :key="header.key"
+            :property="header.key"
+            :label="header.col"
+            width="140"
+          >
           </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
+          <el-table-column fixed="right" label="操作" width="100">
+            <template slot-scope="scope">
+              <el-button
+                @click="handleDownLoad(scope.row)"
+                type="text"
+                size="small"
+                >下载</el-button
+              >
+              <el-button
+                @click="handlePoint(scope.row)"
+                type="text"
+                size="small"
+                >评分</el-button
+              >
+            </template>
           </el-table-column>
-          <el-table-column prop="address" label="地址"> </el-table-column>
-          <p v-for="(index, header) in tableHeader" :key="index">
-            <el-table-column prop="date" :label="header" width="140">
-            </el-table-column>
-          </p>
         </el-table>
       </el-main>
     </el-container>
@@ -89,13 +58,95 @@ export default {
   data() {
     const item = {
         date: "2016-05-02",
-        name: "王小虎",
-        address: "上海市普陀区金沙江路 1518 弄"
+        name: "edf",
+        address: "sad"
       },
-      cols = ["日期", "姓名", "地址"];
+      MenuData = [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      form = 0,
+      switchText1 = "按题目查找",
+      switchText2 = "按学生查找",
+      defaultProps = {
+        children: "children",
+        label: "label"
+      },
+      header = [
+        {
+          col: "日期",
+          key: "date"
+        },
+        {
+          col: "姓名",
+          key: "name"
+        },
+        {
+          col: "地址",
+          key: "address"
+        }
+      ];
     return {
       tableData: Array(20).fill(item),
-      tableHeader: Array(3).fill(cols)
+      tableHeader: header,
+      menu: MenuData,
+      defaultProps: defaultProps,
+      form: form,
+      switchText1: switchText1,
+      switchText2: switchText2
     };
   }
 };
